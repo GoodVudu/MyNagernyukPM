738940fee7e8b10ea02f6e68cea332d60f2cbc1e
GoodVudu <egornager@gmail.com>
2023-03-16 12:14:58 +0200

    Fix all problems with adapter work!

--- app/src/main/java/com/example/mynagernyukpm/FragmentOne.kt
+++ app/src/main/java/com/example/mynagernyukpm/FragmentOne.kt
@@ -6,9 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
-
+import kotlinx.android.synthetic.main.fragment_one.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
